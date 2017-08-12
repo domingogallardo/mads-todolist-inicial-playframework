93d170740d859e3ee359bb9a98ec7a360e23c986
Domingo Gallardo <domingo.gallardo@ua.es>
2017-08-12 12:45:36 +0200

    Actualizado Usuario con apellidos y fecha nacimiento

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,10 +1,14 @@
 package models;
 
+import java.util.Date;
+
 public class Usuario {
    public String login;
    public String email;
    public String password;
    public String nombre;
+   public String apellidos;
+   public Date fechaNacimiento;
 
    public Usuario(String login, String email) {
       this.login = login;
--- test/models/UsuarioTest.java
+++ test/models/UsuarioTest.java
@@ -1,19 +1,29 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 import models.Usuario;
 
 public class UsuarioTest {
 
    @Test
-   public void testCrearUsuario() {
+   public void testCrearUsuario() throws ParseException {
       // Los parámetros del constructor son los campos obligatorios
-      Usuario usuario = new Usuario("pepitoperez", "pepitoperez@gmail.com");
-      usuario.nombre = "Pepito Pérez Fernández";
+      Usuario usuario = new Usuario("juangutierrez", "juangutierrez@gmail.com");
+      usuario.nombre = "Juan";
+      usuario.apellidos = "Gutierrez";
       usuario.password = "123456789";
-      assertEquals("pepitoperez", usuario.login);
-      assertEquals("pepitoperez@gmail.com", usuario.email);
-      assertEquals("Pepito Pérez Fernández", usuario.nombre);
+
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+      usuario.fechaNacimiento = sdf.parse("1997-02-20");
+
+      assertEquals("juangutierrez", usuario.login);
+      assertEquals("juangutierrez@gmail.com", usuario.email);
+      assertEquals("Juan", usuario.nombre);
+      assertEquals("Gutierrez", usuario.apellidos);
       assertEquals("123456789", usuario.password);
+      assertTrue(usuario.fechaNacimiento.compareTo(sdf.parse("1997-02-20")) == 0);
    }
 }
