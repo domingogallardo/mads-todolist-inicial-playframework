f89a3c9a7de50088d2376d3365d7d96690970cd2
Domingo Gallardo <domingo.gallardo@ua.es>
2018-10-08 12:48:47 +0200

    Arreglado bug usuario duplicado

---
+++ app/models/UsuarioRepositoryException.java
@@ -0,0 +1,11 @@
+package models;
+
+public class UsuarioRepositoryException extends RuntimeException {
+
+    public UsuarioRepositoryException() {
+    }
+
+    public UsuarioRepositoryException(String message) {
+        super(message);
+    }
+}
